0fcc732ed690283dd7f4b460ee527461fee888ff
explosiveseccc <98961241+explosiveseccc@users.noreply.github.com>
2023-03-09 12:32:32 -0500

    challenge 4 completed
    challenge 4 is done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,4 +18,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
-        playerRb.AddForce(focalPoint.transform.forward* speed * forwardInput); }
+        playerRb.AddForce(focalPoint.transform.forward* speed * forwardInput); 
+    }
+
+}
